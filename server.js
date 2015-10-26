579950c0390f8626d52301d1e524e90e0ff543b4
tejasrr19 <tejasrr19@gmail.com>
2015-10-25 21:31:51 -0700

    ready to be deployed again

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-// require mongoose config
-var config_mongoose = require('./server/config/mongoose');
-
-// require express module
+/**
+ * Created by Tejas on 10/24/2015.
+ */
 var express = require('express');
-
-// require path module
 var path = require('path');
-
-// create express app
 var app = express();
-
-// require body-parser
 var bodyParser = require('body-parser');
 
-// use body parser (this is newest syntax for instantiation)
-app.use(bodyParser.json()); 
+// mongoose
+var config_mongoose = require('./server/config/mongoose');
+
+app.use(bodyParser.json());
 
 // set path to static folder
 app.use(express.static(path.join(__dirname,'./client')));
 
-// haven't gotten angular file uploads working "angular is not defined" - references are spotty
-// var angularFileUpload = require('angular-file-upload');
-// var multipartform = require('./server/config/multipart');
 
 // require routes config and run
 require('./server/config/routes')(app);
 
-// tell express app to listen on port 3030
+//listen on port 3030
 var port = process.env.PORT || 3030;
 var server = app.listen(port,function() {
 	console.log("listening on port 3030");
